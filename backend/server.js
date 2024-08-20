8310915326e9cc1a2bc8f54dd5c44323f90850f3
SAMI111538 <samiullahglotar420@gamil.com>
2024-08-20 14:03:31 +0500

    add

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const app = require("./app");
+const dotenv = require("dotenv");//simplifies the process of loading environment variables from a '. env' file into Node. js applications, reducing the need for manual configuration.
+const connectDatabase = require("./config/database")
+//Config
+
+dotenv.config({path:"backend/config/config.env"});
+connectDatabase();
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is working on http://localhost:${process.env.PORT}`)
+});
