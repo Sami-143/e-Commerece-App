daf51ffe92bbb14868867a432837a4b4177ef3b9
SAMI111538 <samiullahglotar420@gamil.com>
2024-08-21 22:18:56 +0500

    uploading APi's & Schema

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,30 @@
-exports.getAllProducts = (req,res)=>{
-    res.status(200).json({message:"Route is working fine"})
+const Product = require("../model/productModel");
+
+
+//Create A Product ---Admin Pannel
+exports.createProucts = async (req, res, next) => {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+
+    })
+}
+
+//Getting the Product -- 
+exports.getAllProducts = async (req, res) => {
+    const products = await Product.find();
+    res.status(200).json({
+        success: true,
+        products
+    })
+}
+
+exports.updateProducts = async(req,res)=>{
+    const update = await Product.findById();
+    res.status(500).json({
+        success:true,
+        update
+    })
 }
 
---
+++ backend/model/productModel.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please Enter the Name"],
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: [true, "Please Enter the description"],
+    },
+    price: {
+        type: Number,
+        required: [true, "Please Enter the Price"],
+        maxLength: [6, "Price should not exceed 6 characters"],
+    },
+    rating: {
+        type: Number,
+        default: 0,
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true,
+            },
+            url: {
+                type: String, // This was incorrectly set to `public_id: String`
+                required: true,
+            },
+        }
+    ],
+    category: {
+        type: String,
+        required: [true, "Please Enter the Category"],
+    },
+    Stock: {
+        type: Number,
+        required: [true, "Please Enter the Stock"],
+        maxLength: [4, "Stock should not exceed 4 characters"],
+        default: 1,
+    },
+    numOfReviews: {
+        type: Number,  // Changed from String to Number
+        default: 0,
+    },
+    review: [
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+            comment: {
+                type: String,
+                required: true,
+            },
+            rating: {
+                type: Number,
+                required: true,
+            }
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+module.exports = mongoose.model("Products", productSchema);
