89ae3d81ebd4d4ab395a017dbda4a58d2fd0668b
Sami Ullah <samiullahglotar420@gmail.com>
2024-09-14 22:25:53 +0500

    add

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -14,4 +14,5 @@ exports.isAuthenticatedUser = catchAsyncError(async(req,res,next)=>{
     const decodedData = jwt.verify(token,process.env.JWT_SECRET)
 
     req.user= await User.findById(decodedData.id)
+    
 })
