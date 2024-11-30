a626b2b14342080a9de9907e6cb85e4f21b7b84f
Sami Ullah <samiullahglotar420@gmail.com>
2024-11-30 15:54:39 +0500

    add .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import Header from './Component/layout/Header/Header.js';
 import Footer from './Component/layout/Footer/Footer.js';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import WebFont from "webfontloader";
 import React from 'react';
+import Home from './Component/Home/Home.js';
 
 function App() {
+
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
+
   return (
     <Router>
       <div className="App">
-        <Header/>
-        <Footer/>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+        <Header />
+        <Footer />
       </div>
     </Router>
   );
